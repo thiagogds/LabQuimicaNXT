40abb7bc72bfd3180441c5ef2e4b8d3b361ad1d5
Nilton Lessa <nlessa@gmail.com>
2013-02-18 19:20:08 -0300

    Implementa o calculo de pH.

--- calculators.h
+++ calculators.h
@@ -1,1 +1,27 @@
+#include "app.h"
+#include "cube.h"
+#include "substance.h"
 
+class Calculator{
+public:
+    void mixSubstances();
+    static float calculatePh(CubeBecher *becher);
+};
+
+float Calculator::calculatePh(CubeBecher *becher) {
+    Substance &substance = becher->mixedSubstance;
+
+    // Marreta bionica da quimica!
+    if (substance.molar == 0) {
+        return 7;
+    }
+
+    float e = 2.30258509299404;
+    float ph = -(Sifteo::log (substance.molar)/e);
+
+    if (substance.oh) {
+        ph = 14 - ph;
+    }
+
+    return ph;
+}
