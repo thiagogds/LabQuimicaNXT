1f2b5249300ef1a132b3e3e0e3cd80566d5c25ad
Nilton Lessa <nlessa@gmail.com>
2013-02-18 16:29:47 -0300

    Organiza imagens.

--- assets.lua
+++ assets.lua
@@ -4,12 +4,13 @@ IconAssets = group{quality=9.95}
 Icon = image{"icon.png"}
 
 GameAssets = group{quality=9.95}
-Pipete = image{"pipete.png"}
-Background = image{"white-becher-bkg.png", width=128, height=128}
+Background = image{"background.png", width=128, height=128}
 
 
-SpriteAssets = group{quality=9.95}
-FlaskBkg = image{"flask-transp-bkg.png", width=128, height=128, pinned=true }
-Liquid = image{"dropped-liquid-vertical-transparent-v2.png", width=128, height=128, pinned=true}
-Drop = image{"gota.png", width=32, height=32, pinned=true}
+BecherAssets = group{quality=9.95}
+FlaskBkg = image{"erlenmeyer.png", width=128, height=128, pinned=true }
+Liquid = image{"animated-liquid.png", width=128, height=128, pinned=true}
+Drop = image{"drop.png", width=32, height=32, pinned=true}
 
+PipeteAssets = group{quality=9.95}
+Pipete = image{"pipete.png", height=128, width=128, pinned=true }
